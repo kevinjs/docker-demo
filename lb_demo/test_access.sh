e39d8b8249d49f0cb0d066bcdf8efdd15018a955
kevinjs <dysj4099@gmail.com>
2015-07-06 14:56:57 +0000

    add test access script

---
+++ lb_demo/test_access.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+acc_times=100
+acc_interval=.1
+
+if [ $# -ne 4 ];then
+    echo "Usage:"
+    echo "      `basename $0` -t access_times -i access_interval"
+    exit 0
+fi
+
+acc_times=$2
+acc_interval=$4
+
+echo "for ((i=0;i<$acc_times;i++));do curl -X GET http://127.0.0.1:8080/; sleep $acc_interval; done"
+for ((i=0;i<$acc_times;i++));do curl -X GET http://127.0.0.1:8080/; sleep $acc_interval; done
