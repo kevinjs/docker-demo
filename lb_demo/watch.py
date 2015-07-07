72ee4b1daec8c97d96d8089da602201ac8ab89c2
kevinjs <dysj4099@gmail.com>
2015-07-07 16:29:06 +0000

    add output report

--- lb_demo/watch.py
+++ lb_demo/watch.py
@@ -17,6 +17,37 @@ max_rate = 0
 min_rate = 0
 repeat = {'normal':0, 'abnormal':0}
 
+report = {}
+is_rp = False
+
+def append_file(filename, wrt_line):
+    with open(filename, 'a') as f:
+        f.write('%s\n'%wrt_line)
+
+def create_report(filename):
+    global report
+    #print report
+    col_num = len(report) - 1
+    row_num = len(report['total'])
+    con_ids = list(set(report.keys()).difference(set(['average', 'total', 'start_time', 'end_time'])))
+
+    wrt_title = '%s,%s,%s' %('time', 'total', 'avg')
+    for i in con_ids:
+        wrt_title += ',%s' %i
+    #print wrt_title
+    append_file(filename, wrt_title)
+
+    for i in xrange(0, row_num):
+        wrt_line = '%s,%s,%s' %(i, report['total'][i], report['average'][i])
+        for j in con_ids:
+            value = 0
+            if i in report[j]:
+                value = report[j][i]
+            wrt_line += ',%s' %value
+        #print wrt_line
+        append_file(filename, wrt_line)
+    append_file(filename, '%s,%s'%(report['start_time'], report['end_time']))
+
 def adjust(cli, cur_rate):
     global repeat
     global max_rate
@@ -73,6 +104,13 @@ def main(cli):
     global rate
     global max_rate
     global min_rate
+    global report
+    global is_rp
+
+    if is_rp:
+        report['start_time'] = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        report['total'] = {}
+        report['average'] = {}
 
     cnt_total = 0
     while(True):
@@ -86,6 +124,9 @@ def main(cli):
             cnt_new = dict(zip(cons, [0 for x in cons]))
 
         for i in cons:
+            if is_rp and (i not in report):
+                report[i] = {}
+
             con_ip = cli.inspect_container(i)['NetworkSettings']['IPAddress']
             acc_num = 0
             acc_rate = 0
@@ -120,6 +161,8 @@ def main(cli):
                 else:
                     acc_rate = 0
 	    print '%s	%s	%s	%s'%(i,  con_ip, acc_num, acc_rate)
+            if is_rp:
+                report[i][cnt_total] = acc_rate
 
         rate_avg = 0.0
         length_cons = 1
@@ -127,7 +170,14 @@ def main(cli):
             if cons:
                 length_cons = len(cons)
             rate_avg = float(sum(rate.values()))/length_cons
+            if is_rp:
+                report['total'][cnt_total] = sum(rate.values())
+        else:
+            if is_rp:
+                report['total'][cnt_total] = 0
         print 'total num: %s, average rate: %s' %(len(cons), rate_avg)
+        if is_rp:
+            report['average'][cnt_total] = rate_avg
         if tik == 3:
             auto_adjust(cli, rate_avg)
         print '==' * 10
@@ -145,6 +195,16 @@ if __name__=='__main__':
             pass
     elif len(sys.argv) == 3:
         if 'auto' == sys.argv[1]:
+            is_rp = False
+            tmp = sys.argv[2].split('-')
+            min_rate = float(tmp[0])
+            max_rate = float(tmp[1])
+            if min_rate > max_rate:
+                t = min_rate
+                min_rate = max_rate
+                max_rate = t
+        elif 'report' == sys.argv[1]:
+            is_rp = True
             tmp = sys.argv[2].split('-')
             min_rate = float(tmp[0])
             max_rate = float(tmp[1])
@@ -157,6 +217,11 @@ if __name__=='__main__':
     try:
         main(cli)
     except KeyboardInterrupt:
+        if is_rp:
+            t_t = time.time()
+            report['end_time'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(t_t))
+            filename = 'report_%s.csv' %(time.strftime('%Y%m%d%H%M%S', time.localtime(t_t)))
+            create_report(filename)
         print 'Bye~~~'
         sys.exit(0)
 
